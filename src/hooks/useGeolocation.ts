07a8f3138261820787ef9fe87e7449e5350c9dce
Felipe <felipebergamin6@gmail.com>
2020-10-10 16:20:57 -0300

    fix: hook error handling and index page presentation

--- src/hooks/useGeolocation.ts
+++ src/hooks/useGeolocation.ts
@@ -14,10 +14,26 @@ export default function useGeolocation() {
     } else {
       navigator.permissions.query({ name: 'geolocation' }).then((result) => {
         if (result.state === 'prompt' || result.state === 'granted') {
-          navigator.geolocation.getCurrentPosition((position) => {
-            setLocation(position);
-            setIsLoading(false);
-          });
+          navigator.geolocation.getCurrentPosition(
+            (position) => {
+              setLocation(position);
+              setIsLoading(false);
+            },
+            (err) => {
+              if (err.code === 1)
+                setError(
+                  'You denied location access. But we need it to provide weather data',
+                );
+              else
+                setError(
+                  "Your position is not available. We can't provide weather data",
+                );
+              setIsLoading(false);
+            },
+          );
+        } else {
+          setIsLoading(false);
+          setError('We need to access your location to provide weather data');
         }
       });
     }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 
+import { Grid, Typography } from '~/components';
 import useGeolocation from '../hooks/useGeolocation';
 
 function Index() {
@@ -19,7 +20,19 @@ function Index() {
     }
   }, [location, router]);
 
-  return <h1>Loading...</h1>;
+  return (
+    <Grid
+      style={{ height: '100%' }}
+      container
+      alignContent="center"
+      justify="center"
+    >
+      <Grid item container alignContent="center" justify="center">
+        {isLoading && <Typography variant="h3">Loading...</Typography>}
+        {error && <Typography variant="body1">{error}</Typography>}
+      </Grid>
+    </Grid>
+  );
 }
 
 export default Index;
